474aabef1034bed8497524914bf0901f7e938f28
Enkei92 <yukhnevich92@gmail.com>
2018-06-20 20:56:01 +0300

    feat(users): add devise authentication, user model, bootstrap init

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def show; end
+
+  def update
+    if user.update(user_params)
+      redirect_to user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  attr_reader :user
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
+end
