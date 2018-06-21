ff7e60bbe9a4e9acaa24b9996e50c0d6fd8c7da5
Enkei92 <yukhnevich92@gmail.com>
2018-06-21 10:27:13 +0300

    chore: add period validates for projects

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.6'
+gem 'rails', '~> 5.2.0'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
@@ -37,6 +37,8 @@ gem 'bootstrap', '~> 4.1.1'
 gem 'cancancan'
 gem 'date_validator'
 gem 'devise'
+gem 'jquery-rails'
+gem 'sprockets-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/project.rb
+++ app/models/project.rb
@@ -6,6 +6,11 @@ class Project < ApplicationRecord
                          date: { after: proc { Time.now - 1.year },
                                  before: proc { Time.now + 1.year } }
   validates :end_date, presence: true,
-                       date: { after: proc { Time.now - 1.month },
+                       date: { after: proc { Time.now + 1.month },
                                before: proc { Time.now + 1.year } }
+  validate :period_must_be_valid
+
+  def period_must_be_valid
+    errors.add(:end_date, "start date can't be after end date") if start_date < end_date
+  end
 end
